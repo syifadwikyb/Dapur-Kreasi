80d42a931612d0ad44e1f079aab37fc21fb67efb
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2024-12-02 14:23:39 +0700

    first commit

---
+++ resources/views/layouts/landingpage.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="{{ asset('asset/headerico.png') }}">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="bg-yellow-600 font-jaldi">
+    <x-content></x-content>
+</body>
+</html>
